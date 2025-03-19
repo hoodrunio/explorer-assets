d856c0d2f6571e4ef5ea0a3fa168dc2b87d572ab
omercanyenigun <77763663+omercanyenigun@users.noreply.github.com>
2025-03-19 22:45:23 +0300

    Update tr.sh

--- testnet/layeredge/tr.sh
+++ testnet/layeredge/tr.sh
@@ -80,11 +80,11 @@ cd light-node
 
 echo "Ortam değişkenleri ayarlanıyor..."
 cat <<EOF > .env
-GRPC_URL=34.31.74.109:9090
+GRPC_URL=https://layeredge.mintair.xyz/
 CONTRACT_ADDR=cosmos1ufs3tlq4umljk0qfe8k5ya0x6hpavn897u2cnf9k0en9jr7qarqqt56709
 ZK_PROVER_URL=http://127.0.0.1:3001
 API_REQUEST_TIMEOUT=100
-POINTS_API=http://127.0.0.1:8080
+POINTS_API=http:https://light-node.layeredge.io
 PRIVATE_KEY='$PRIVATE_KEY'
 EOF
 
