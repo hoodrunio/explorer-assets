e84a1ba4251ab7604e61eabdac9f0ff20d270466
Errorist <71728280+Errorist79@users.noreply.github.com>
2025-03-20 23:18:05 +0300

    remove risc0 merkle service

--- testnet/layeredge/tr.sh
+++ testnet/layeredge/tr.sh
@@ -24,12 +24,6 @@ sudo apt update && sudo apt upgrade -y
 echo "Bağımlılıklar kuruluyor..."
 sudo apt update && sudo apt install -y build-essential clang pkg-config
 
-echo "Docker kuruluyor..."
-sudo apt install -y docker.io
-sudo systemctl enable --now docker
-sudo usermod -aG docker $(whoami)
-docker --version
-
 echo "Go kuruluyor..."
 wget https://go.dev/dl/go1.22.1.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.22.1.linux-amd64.tar.gz
@@ -38,41 +32,6 @@ source $HOME/.profile
 go version
 # Temizlik
 rm -f go1.22.1.linux-amd64.tar.gz
-
-echo "Rust ve Risc0 kuruluyor..."
-curl -L https://sh.rustup.rs -o rustup-init.sh
-chmod +x rustup-init.sh
-./rustup-init.sh -y
-source "$HOME/.cargo/env"
-
-echo "Risc0 kurulum dosyaları indiriliyor..."
-curl -L https://risczero.com/install -o risc0-install.sh
-chmod +x risc0-install.sh
-./risc0-install.sh
-
-# PATH değişkenlerini güncelleme
-echo 'export PATH="$HOME/.risc0/bin:$PATH"' >> $HOME/.profile
-echo 'export PATH="$HOME/.risc0/bin:$PATH"' >> $HOME/.bashrc
-if [ -f "$HOME/.zshrc" ]; then
-    echo 'export PATH="$HOME/.risc0/bin:$PATH"' >> $HOME/.zshrc
-fi
-
-# Shell kaynağını yenileme
-source $HOME/.profile
-if [ -f "$HOME/.bashrc" ]; then
-    source "$HOME/.bashrc"
-fi
-if [ -f "$HOME/.zshrc" ]; then
-    source "$HOME/.zshrc"
-fi
-
-# Temizlik
-rm -f rustup-init.sh risc0-install.sh
-
-echo "Risc0 araçları kuruluyor..."
-export PATH="$HOME/.risc0/bin:$PATH"
-rzup install
-rzup --version
 
 echo "Repo klonlanıyor..."
 git clone https://github.com/Layer-Edge/light-node.git
@@ -87,40 +46,6 @@ API_REQUEST_TIMEOUT=100
 POINTS_API=https://light-node.layeredge.io
 PRIVATE_KEY='$PRIVATE_KEY'
 EOF
-
-echo "Risc0 ve Light Node hazırlanıyor..."
-cd $HOME/light-node
-chmod +x scripts/build-risczero.sh
-export PATH="$HOME/.risc0/bin:$PATH"
-./scripts/build-risczero.sh
-if [ $? -ne 0 ]; then
-    echo "Risc0 derleme hatası. Lütfen hata mesajlarını kontrol edin."
-    exit 1
-fi
-sleep 5
-go build
-
-echo "Risc0 Servis dosyası oluşturuluyor..."
-sudo tee /etc/systemd/system/risc0.service <<EOF
-[Unit]
-Description=Risc0
-After=network.target
-
-[Service]
-Type=simple
-User=$(whoami)
-WorkingDirectory=$HOME/light-node
-ExecStart=$HOME/light-node/risc0-merkle-service/target/release/host
-Restart=always
-RestartSec=5
-Environment="PATH=/usr/local/go/bin:/usr/bin:/bin:$HOME/.cargo/bin:$HOME/.risc0/bin"
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-sudo systemctl enable risc0
-sudo systemctl start risc0
 
 echo "Light Node Servis dosyası oluşturuluyor..."
 sudo tee /etc/systemd/system/layer-edge.service <<EOF
@@ -150,5 +75,4 @@ sudo systemctl start layer-edge
 echo -e "\033[32mKurulum tamamlandı!\033[0m"
 echo -e "\033[34mLogları görüntülemek için:\033[0m"
 echo -e "\033[33mLight Node:\033[0m journalctl -fo cat -u layer-edge \033[35m(yüklenmesi için biraz bekleyin)\033[0m"
-echo -e "\033[33mRisc0:\033[0m journalctl -fo cat -u risc0 \033[35m(yüklenmesi için biraz bekleyin)\033[0m"
 echo -e "\033[34mServis durumunu kontrol etmek için:\033[0m sudo systemctl status layer-edge"
